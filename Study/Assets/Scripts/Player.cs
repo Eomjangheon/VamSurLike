a5525082229f00b3423035d6a7aab167bf99295e
Eomjangheon <mpmpm0610@gmail.com>
2023-09-19 11:44:02 +0900

    Feat : Player animator
    좌우 이동시 캐릭터 플립
    플레이어 애니메이터 생성
    애니메이터 파라미터 설정

--- Study/Assets/Scripts/Player.cs
+++ Study/Assets/Scripts/Player.cs
@@ -11,9 +11,13 @@ public class Player : MonoBehaviour
     float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator animator;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         speed = 3;
     }
 
@@ -28,5 +32,16 @@ public class Player : MonoBehaviour
 	{
         Vector2 nextVec=inputVec.normalized * speed* Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+        
+	}
+
+	void LateUpdate()
+	{
+		animator.SetFloat("Speed", inputVec.magnitude);
+
+		if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
 	}
 }
